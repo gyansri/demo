7f18f44e59452a6243bea2b261d42fb3c19c8a41
gps <you@example.com>
2020-01-05 20:50:45 +0530

    First Commit

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+public class Person implements AB {
+    private int age;
+    private int id;
+
+
+    public void test() {
+
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("a = " + a);
+        System.out.println("args = " + args);
+    }
+
+    public Person(int age, int id) {
+        this.age = age;
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
+interface AB {
+    void test();
+}
